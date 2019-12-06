7116074a39f68af5ae69a3beb9ee93f9473d9e01
himustofa <mustofa2008@gmail.com>
2019-12-06 09:05:02 +0600

    Initial commit

--- app/src/main/java/com/mk/springrestapi/activities/MainActivity.java
+++ app/src/main/java/com/mk/springrestapi/activities/MainActivity.java
@@ -1,23 +1,39 @@
 package com.mk.springrestapi.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.gson.Gson;
 import com.mk.springrestapi.R;
 import com.mk.springrestapi.models.Student;
 import com.mk.springrestapi.retrofit.RetrofitClient;
+import com.mk.springrestapi.utils.Utility;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,17 +42,51 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int ACTION_PICK_REQUEST_CODE = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.image_view1);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, ACTION_PICK_REQUEST_CODE);
+            }
+        });
+
         //addStudent();
         //getAllStudents();
         //getStudentById();
         //updateStudentById();
-        deleteStudentById();
+        //deleteStudentById();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==ACTION_PICK_REQUEST_CODE && resultCode==RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            //imageView.setImageURI(uri);
+            //imageName = "img_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date()) + ".png";
+            //Bitmap bm = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                if (bitmap != null) {
+                    String imagePath = Utility.saveToInternalStorage(MainActivity.this, bitmap, "sample");
+                    imageView.setImageBitmap(Utility.loadImage(imagePath, "sample"));
+                    uploadToServer(imagePath, "sample");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     private void addStudent() {
@@ -138,6 +188,34 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(@NonNull Call<Boolean> call, @NonNull Throwable t) {
+                Log.e(TAG, "Retrofit " + t.getMessage());
+            }
+        });
+    }
+
+    private void uploadToServer(String filePath, String imageName) {
+        //Create a file object using file path
+        //File file = new File(filePath);
+        File file = new File(filePath, imageName);
+        // Create a request body with file and image media type
+        RequestBody fileReqBody = RequestBody.create(MediaType.parse("image/*"), file);
+        // Create MultipartBody.Part using file request-body,file name and part name
+        MultipartBody.Part part = MultipartBody.Part.createFormData("upload", file.getName(), fileReqBody);
+        //Create request body with text description and text media type
+        RequestBody description = RequestBody.create(MediaType.parse("text/plain"), "image-type");
+        Call<ResponseBody> res = RetrofitClient.getInstance().getApi().uploadImage(part, description);
+        res.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        Log.d(TAG, "Retrofit " + new Gson().toJson(response.body()));
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
                 Log.e(TAG, "Retrofit " + t.getMessage());
             }
         });
--- app/src/main/java/com/mk/springrestapi/retrofit/Api.java
+++ app/src/main/java/com/mk/springrestapi/retrofit/Api.java
@@ -5,19 +5,19 @@ import com.mk.springrestapi.models.Student;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface Api {
 
@@ -49,4 +49,9 @@ public interface Api {
 
     @DELETE("student/delete/{id}")
     Call<Boolean> deleteById(@Path("id") String id);
+
+    @Multipart
+    @POST("/upload")
+    //Call<ResponseBody> uploadImage(@Part MultipartBody.Part file);
+    Call<ResponseBody> uploadImage(@Part MultipartBody.Part file, @Part("name") RequestBody requestBody);
 }
